0256ec941fcd1f3e83e91063ed3c81720c4788e8
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:28:48 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EShift_transport
+{
+    public partial class Product : Form
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadmin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdminHome home = new AdminHome();
+            home.Show();
+        }
+    }
+}
