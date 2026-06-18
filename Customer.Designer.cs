0df8a4d9ecd2c5548f4600e8e5146cd8dd40246e
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:13:29 +0530

    Add files via upload

---
+++ Customer.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace EShift_transport
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Customer));
+            this.dataGridViewcustomerdetils = new System.Windows.Forms.DataGridView();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblrequesteddate = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtcustomername = new System.Windows.Forms.TextBox();
+            this.lblcustomername = new System.Windows.Forms.Label();
+            this.btnimage = new System.Windows.Forms.Button();
+            this.lbladmin = new System.Windows.Forms.Label();
+            this.lbleshift = new System.Windows.Forms.Label();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btnjob = new System.Windows.Forms.Button();
+            this.btncustomer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewcustomerdetils)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewcustomerdetils
+            // 
+            this.dataGridViewcustomerdetils.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewcustomerdetils.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewcustomerdetils.GridColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewcustomerdetils.Location = new System.Drawing.Point(330, 381);
+            this.dataGridViewcustomerdetils.Name = "dataGridViewcustomerdetils";
+            this.dataGridViewcustomerdetils.RowHeadersWidth = 51;
+            this.dataGridViewcustomerdetils.RowTemplate.Height = 24;
+            this.dataGridViewcustomerdetils.Size = new System.Drawing.Size(1052, 280);
+            this.dataGridViewcustomerdetils.TabIndex = 245;
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch.Location = new System.Drawing.Point(946, 260);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(124, 43);
+            this.btnsearch.TabIndex = 244;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1200, 196);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(257, 22);
+            this.dateTimePicker1.TabIndex = 243;
+            // 
+            // lblrequesteddate
+            // 
+            this.lblrequesteddate.AutoSize = true;
+            this.lblrequesteddate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblrequesteddate.Location = new System.Drawing.Point(941, 191);
+            this.lblrequesteddate.Name = "lblrequesteddate";
+            this.lblrequesteddate.Size = new System.Drawing.Size(201, 29);
+            this.lblrequesteddate.TabIndex = 242;
+            this.lblrequesteddate.Text = "Requested Date";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1112, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 43);
+            this.button2.TabIndex = 241;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtcustomername
+            // 
+            this.txtcustomername.Location = new System.Drawing.Point(611, 191);
+            this.txtcustomername.Name = "txtcustomername";
+            this.txtcustomername.Size = new System.Drawing.Size(257, 22);
+            this.txtcustomername.TabIndex = 240;
+            // 
+            // lblcustomername
+            // 
+            this.lblcustomername.AutoSize = true;
+            this.lblcustomername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcustomername.Location = new System.Drawing.Point(352, 184);
+            this.lblcustomername.Name = "lblcustomername";
+            this.lblcustomername.Size = new System.Drawing.Size(201, 29);
+            this.lblcustomername.TabIndex = 239;
+            this.lblcustomername.Text = "Customer Name";
+            // 
+            // btnimage
+            // 
+            this.btnimage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnimage.Image = ((System.Drawing.Image)(resources.GetObject("btnimage.Image")));
+            this.btnimage.Location = new System.Drawing.Point(21, 23);
+            this.btnimage.Name = "btnimage";
+            this.btnimage.Size = new System.Drawing.Size(207, 160);
+            this.btnimage.TabIndex = 238;
+            this.btnimage.UseVisualStyleBackColor = true;
+            // 
+            // lbladmin
+            // 
+            this.lbladmin.AutoSize = true;
+            this.lbladmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbladmin.Location = new System.Drawing.Point(735, 56);
+            this.lbladmin.Name = "lbladmin";
+            this.lbladmin.Size = new System.Drawing.Size(159, 36);
+            this.lbladmin.TabIndex = 237;
+            this.lbladmin.Text = "Customer ";
+            // 
+            // lbleshift
+            // 
+            this.lbleshift.AutoSize = true;
+            this.lbleshift.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleshift.Location = new System.Drawing.Point(244, 23);
+            this.lbleshift.Name = "lbleshift";
+            this.lbleshift.Size = new System.Drawing.Size(275, 91);
+            this.lbleshift.TabIndex = 236;
+            this.lbleshift.Text = "e-Shift";
+            // 
+            // btnload
+            // 
+            this.btnload.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnload.Location = new System.Drawing.Point(21, 420);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(190, 39);
+            this.btnload.TabIndex = 235;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            // 
+            // btnjob
+            // 
+            this.btnjob.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnjob.Location = new System.Drawing.Point(21, 353);
+            this.btnjob.Name = "btnjob";
+            this.btnjob.Size = new System.Drawing.Size(190, 39);
+            this.btnjob.TabIndex = 234;
+            this.btnjob.Text = "Transport Job";
+            this.btnjob.UseVisualStyleBackColor = true;
+            // 
+            // btncustomer
+            // 
+            this.btncustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncustomer.Location = new System.Drawing.Point(21, 280);
+            this.btncustomer.Name = "btncustomer";
+            this.btncustomer.Size = new System.Drawing.Size(190, 39);
+            this.btncustomer.TabIndex = 233;
+            this.btncustomer.Text = "Customer";
+            this.btncustomer.UseVisualStyleBackColor = true;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1515, 713);
+            this.Controls.Add(this.dataGridViewcustomerdetils);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblrequesteddate);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtcustomername);
+            this.Controls.Add(this.lblcustomername);
+            this.Controls.Add(this.btnimage);
+            this.Controls.Add(this.lbladmin);
+            this.Controls.Add(this.lbleshift);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.btnjob);
+            this.Controls.Add(this.btncustomer);
+            this.Name = "Customer";
+            this.Text = "Customer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewcustomerdetils)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewcustomerdetils;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblrequesteddate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtcustomername;
+        private System.Windows.Forms.Label lblcustomername;
+        private System.Windows.Forms.Button btnimage;
+        private System.Windows.Forms.Label lbladmin;
+        private System.Windows.Forms.Label lbleshift;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btnjob;
+        private System.Windows.Forms.Button btncustomer;
+    }
+}
