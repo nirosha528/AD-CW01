dff16e5f5d0881d62efd30dd62bb7ab07f12a429
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:12:18 +0530

    Add files via upload

---
+++ AdminSignup.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace EShift_transport
+{
+    partial class AdminSignup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminSignup));
+            this.button2 = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.cmbuserid = new System.Windows.Forms.ComboBox();
+            this.btnimage = new System.Windows.Forms.Button();
+            this.lbleshift = new System.Windows.Forms.Label();
+            this.dataGridViewadminsign = new System.Windows.Forms.DataGridView();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtuserid = new System.Windows.Forms.TextBox();
+            this.lbluserid = new System.Windows.Forms.Label();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.lblsignup = new System.Windows.Forms.Label();
+            this.btnstaff = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewadminsign)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1035, 646);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 43);
+            this.button2.TabIndex = 178;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(689, 646);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(124, 43);
+            this.btndelete.TabIndex = 177;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(858, 646);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(124, 43);
+            this.btnupdate.TabIndex = 176;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(532, 646);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 43);
+            this.btnAdd.TabIndex = 175;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch.Location = new System.Drawing.Point(1311, 161);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(124, 43);
+            this.btnsearch.TabIndex = 174;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // cmbuserid
+            // 
+            this.cmbuserid.FormattingEnabled = true;
+            this.cmbuserid.Location = new System.Drawing.Point(1056, 175);
+            this.cmbuserid.Name = "cmbuserid";
+            this.cmbuserid.Size = new System.Drawing.Size(183, 24);
+            this.cmbuserid.TabIndex = 173;
+            // 
+            // btnimage
+            // 
+            this.btnimage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnimage.Image = ((System.Drawing.Image)(resources.GetObject("btnimage.Image")));
+            this.btnimage.Location = new System.Drawing.Point(12, 23);
+            this.btnimage.Name = "btnimage";
+            this.btnimage.Size = new System.Drawing.Size(207, 160);
+            this.btnimage.TabIndex = 172;
+            this.btnimage.UseVisualStyleBackColor = true;
+            // 
+            // lbleshift
+            // 
+            this.lbleshift.AutoSize = true;
+            this.lbleshift.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleshift.Location = new System.Drawing.Point(234, 49);
+            this.lbleshift.Name = "lbleshift";
+            this.lbleshift.Size = new System.Drawing.Size(275, 91);
+            this.lbleshift.TabIndex = 171;
+            this.lbleshift.Text = "e-Shift";
+            // 
+            // dataGridViewadminsign
+            // 
+            this.dataGridViewadminsign.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewadminsign.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewadminsign.Location = new System.Drawing.Point(923, 237);
+            this.dataGridViewadminsign.Name = "dataGridViewadminsign";
+            this.dataGridViewadminsign.RowHeadersWidth = 51;
+            this.dataGridViewadminsign.RowTemplate.Height = 24;
+            this.dataGridViewadminsign.Size = new System.Drawing.Size(512, 384);
+            this.dataGridViewadminsign.TabIndex = 170;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(611, 326);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(222, 22);
+            this.txtusername.TabIndex = 169;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(611, 418);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(222, 22);
+            this.txtpassword.TabIndex = 168;
+            // 
+            // txtuserid
+            // 
+            this.txtuserid.Location = new System.Drawing.Point(611, 244);
+            this.txtuserid.Name = "txtuserid";
+            this.txtuserid.Size = new System.Drawing.Size(222, 22);
+            this.txtuserid.TabIndex = 167;
+            // 
+            // lbluserid
+            // 
+            this.lbluserid.AutoSize = true;
+            this.lbluserid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluserid.Location = new System.Drawing.Point(388, 237);
+            this.lbluserid.Name = "lbluserid";
+            this.lbluserid.Size = new System.Drawing.Size(97, 29);
+            this.lbluserid.TabIndex = 166;
+            this.lbluserid.Text = "User Id";
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblusername.Location = new System.Drawing.Point(388, 319);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(144, 29);
+            this.lblusername.TabIndex = 165;
+            this.lblusername.Text = "User Name";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpassword.Location = new System.Drawing.Point(388, 411);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(128, 29);
+            this.lblpassword.TabIndex = 164;
+            this.lblpassword.Text = "Password";
+            // 
+            // lblsignup
+            // 
+            this.lblsignup.AutoSize = true;
+            this.lblsignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsignup.Location = new System.Drawing.Point(618, 83);
+            this.lblsignup.Name = "lblsignup";
+            this.lblsignup.Size = new System.Drawing.Size(229, 36);
+            this.lblsignup.TabIndex = 163;
+            this.lblsignup.Text = "Admin Sign Up";
+            // 
+            // btnstaff
+            // 
+            this.btnstaff.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstaff.Location = new System.Drawing.Point(385, 646);
+            this.btnstaff.Name = "btnstaff";
+            this.btnstaff.Size = new System.Drawing.Size(124, 43);
+            this.btnstaff.TabIndex = 162;
+            this.btnstaff.Text = "Login";
+            this.btnstaff.UseVisualStyleBackColor = true;
+            // 
+            // AdminSignup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1507, 714);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.cmbuserid);
+            this.Controls.Add(this.btnimage);
+            this.Controls.Add(this.lbleshift);
+            this.Controls.Add(this.dataGridViewadminsign);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtuserid);
+            this.Controls.Add(this.lbluserid);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.lblpassword);
+            this.Controls.Add(this.lblsignup);
+            this.Controls.Add(this.btnstaff);
+            this.Name = "AdminSignup";
+            this.Text = "AdminSignup";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewadminsign)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.ComboBox cmbuserid;
+        private System.Windows.Forms.Button btnimage;
+        private System.Windows.Forms.Label lbleshift;
+        private System.Windows.Forms.DataGridView dataGridViewadminsign;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtuserid;
+        private System.Windows.Forms.Label lbluserid;
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.Label lblsignup;
+        private System.Windows.Forms.Button btnstaff;
+    }
+}
