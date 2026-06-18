47d77b2742508ea776ea7e2ea8e15e7168fcb321
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:30:32 +0530

    Add files via upload

---
+++ Report.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EShift_transport
+{
+    public partial class Report : Form
+    {
+        public Report()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadmin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdminHome home = new AdminHome();
+            home.Show();
+        }
+    }
+}
