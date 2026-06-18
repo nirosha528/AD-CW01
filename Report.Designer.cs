28614850df6cdd9610b410806db072a240f43d2d
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:30:47 +0530

    Add files via upload

---
+++ Report.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace EShift_transport
+{
+    partial class Report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Report));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblrequesteddate = new System.Windows.Forms.Label();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnproduct = new System.Windows.Forms.Button();
+            this.btnimage = new System.Windows.Forms.Button();
+            this.lblReport = new System.Windows.Forms.Label();
+            this.lbleshift = new System.Windows.Forms.Label();
+            this.btnreport = new System.Windows.Forms.Button();
+            this.btntransunit = new System.Windows.Forms.Button();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btnjob = new System.Windows.Forms.Button();
+            this.btncustomer = new System.Windows.Forms.Button();
+            this.btnadmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1056, 160);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(257, 22);
+            this.dateTimePicker1.TabIndex = 251;
+            // 
+            // lblrequesteddate
+            // 
+            this.lblrequesteddate.AutoSize = true;
+            this.lblrequesteddate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblrequesteddate.Location = new System.Drawing.Point(790, 153);
+            this.lblrequesteddate.Name = "lblrequesteddate";
+            this.lblrequesteddate.Size = new System.Drawing.Size(201, 29);
+            this.lblrequesteddate.TabIndex = 250;
+            this.lblrequesteddate.Text = "Requested Date";
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch.Location = new System.Drawing.Point(1375, 153);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(124, 43);
+            this.btnsearch.TabIndex = 249;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(533, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 22);
+            this.textBox1.TabIndex = 248;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 29);
+            this.label1.TabIndex = 247;
+            this.label1.Text = "Customer Id";
+            // 
+            // btnproduct
+            // 
+            this.btnproduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnproduct.Location = new System.Drawing.Point(12, 337);
+            this.btnproduct.Name = "btnproduct";
+            this.btnproduct.Size = new System.Drawing.Size(190, 39);
+            this.btnproduct.TabIndex = 246;
+            this.btnproduct.Text = "Product";
+            this.btnproduct.UseVisualStyleBackColor = true;
+            // 
+            // btnimage
+            // 
+            this.btnimage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnimage.Image = ((System.Drawing.Image)(resources.GetObject("btnimage.Image")));
+            this.btnimage.Location = new System.Drawing.Point(12, 12);
+            this.btnimage.Name = "btnimage";
+            this.btnimage.Size = new System.Drawing.Size(207, 160);
+            this.btnimage.TabIndex = 245;
+            this.btnimage.UseVisualStyleBackColor = true;
+            // 
+            // lblReport
+            // 
+            this.lblReport.AutoSize = true;
+            this.lblReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReport.Location = new System.Drawing.Point(683, 48);
+            this.lblReport.Name = "lblReport";
+            this.lblReport.Size = new System.Drawing.Size(111, 36);
+            this.lblReport.TabIndex = 244;
+            this.lblReport.Text = "Report";
+            // 
+            // lbleshift
+            // 
+            this.lbleshift.AutoSize = true;
+            this.lbleshift.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleshift.Location = new System.Drawing.Point(243, 17);
+            this.lbleshift.Name = "lbleshift";
+            this.lbleshift.Size = new System.Drawing.Size(275, 91);
+            this.lbleshift.TabIndex = 243;
+            this.lbleshift.Text = "e-Shift";
+            // 
+            // btnreport
+            // 
+            this.btnreport.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreport.Location = new System.Drawing.Point(12, 585);
+            this.btnreport.Name = "btnreport";
+            this.btnreport.Size = new System.Drawing.Size(190, 39);
+            this.btnreport.TabIndex = 242;
+            this.btnreport.Text = "Report";
+            this.btnreport.UseVisualStyleBackColor = true;
+            // 
+            // btntransunit
+            // 
+            this.btntransunit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntransunit.Location = new System.Drawing.Point(12, 525);
+            this.btntransunit.Name = "btntransunit";
+            this.btntransunit.Size = new System.Drawing.Size(190, 39);
+            this.btntransunit.TabIndex = 241;
+            this.btntransunit.Text = "Transport Unit";
+            this.btntransunit.UseVisualStyleBackColor = true;
+            // 
+            // btnload
+            // 
+            this.btnload.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnload.Location = new System.Drawing.Point(12, 462);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(190, 39);
+            this.btnload.TabIndex = 240;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            // 
+            // btnjob
+            // 
+            this.btnjob.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnjob.Location = new System.Drawing.Point(12, 395);
+            this.btnjob.Name = "btnjob";
+            this.btnjob.Size = new System.Drawing.Size(190, 39);
+            this.btnjob.TabIndex = 239;
+            this.btnjob.Text = "Transport Job";
+            this.btnjob.UseVisualStyleBackColor = true;
+            // 
+            // btncustomer
+            // 
+            this.btncustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncustomer.Location = new System.Drawing.Point(12, 269);
+            this.btncustomer.Name = "btncustomer";
+            this.btncustomer.Size = new System.Drawing.Size(190, 39);
+            this.btncustomer.TabIndex = 238;
+            this.btncustomer.Text = "Customer";
+            this.btncustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnadmin
+            // 
+            this.btnadmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadmin.Location = new System.Drawing.Point(12, 207);
+            this.btnadmin.Name = "btnadmin";
+            this.btnadmin.Size = new System.Drawing.Size(190, 39);
+            this.btnadmin.TabIndex = 237;
+            this.btnadmin.Text = "Admin";
+            this.btnadmin.UseVisualStyleBackColor = true;
+            this.btnadmin.Click += new System.EventHandler(this.btnadmin_Click);
+            // 
+            // Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1521, 697);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblrequesteddate);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnproduct);
+            this.Controls.Add(this.btnimage);
+            this.Controls.Add(this.lblReport);
+            this.Controls.Add(this.lbleshift);
+            this.Controls.Add(this.btnreport);
+            this.Controls.Add(this.btntransunit);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.btnjob);
+            this.Controls.Add(this.btncustomer);
+            this.Controls.Add(this.btnadmin);
+            this.Name = "Report";
+            this.Text = "Report";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblrequesteddate;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnproduct;
+        private System.Windows.Forms.Button btnimage;
+        private System.Windows.Forms.Label lblReport;
+        private System.Windows.Forms.Label lbleshift;
+        private System.Windows.Forms.Button btnreport;
+        private System.Windows.Forms.Button btntransunit;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btnjob;
+        private System.Windows.Forms.Button btncustomer;
+        private System.Windows.Forms.Button btnadmin;
+    }
+}
