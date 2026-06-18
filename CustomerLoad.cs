9618fd73ba930d7505808654f5c9450e16f92894
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:17:46 +0530

    Add files via upload

---
+++ CustomerLoad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EShift_transport
+{
+    public partial class CustomerLoad : Form
+    {
+        public CustomerLoad()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
