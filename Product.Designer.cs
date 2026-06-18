673935b0c8aa7ba83f4bab957896406901edd523
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:29:15 +0530

    Add files via upload

---
+++ Product.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace EShift_transport
+{
+    partial class Product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Product));
+            this.dataGridViewproductdetails = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtdescription = new System.Windows.Forms.TextBox();
+            this.lbldescription = new System.Windows.Forms.Label();
+            this.txtproductid = new System.Windows.Forms.TextBox();
+            this.lblproductid = new System.Windows.Forms.Label();
+            this.txtproductname = new System.Windows.Forms.TextBox();
+            this.lblproductname = new System.Windows.Forms.Label();
+            this.btnproduct = new System.Windows.Forms.Button();
+            this.btnimage = new System.Windows.Forms.Button();
+            this.lblproductdetails = new System.Windows.Forms.Label();
+            this.lbleshift = new System.Windows.Forms.Label();
+            this.btnreport = new System.Windows.Forms.Button();
+            this.btntransunit = new System.Windows.Forms.Button();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btnjob = new System.Windows.Forms.Button();
+            this.btncustomer = new System.Windows.Forms.Button();
+            this.btnadmin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewproductdetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewproductdetails
+            // 
+            this.dataGridViewproductdetails.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewproductdetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewproductdetails.Location = new System.Drawing.Point(309, 462);
+            this.dataGridViewproductdetails.Name = "dataGridViewproductdetails";
+            this.dataGridViewproductdetails.RowHeadersWidth = 51;
+            this.dataGridViewproductdetails.RowTemplate.Height = 24;
+            this.dataGridViewproductdetails.Size = new System.Drawing.Size(1117, 150);
+            this.dataGridViewproductdetails.TabIndex = 250;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(508, 398);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 22);
+            this.textBox1.TabIndex = 249;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 29);
+            this.label1.TabIndex = 248;
+            this.label1.Text = "Product Id";
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch.Location = new System.Drawing.Point(837, 391);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(124, 43);
+            this.btnsearch.TabIndex = 247;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1338, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 43);
+            this.button2.TabIndex = 246;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(1005, 245);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(124, 43);
+            this.btndelete.TabIndex = 245;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(1174, 245);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(124, 43);
+            this.btnupdate.TabIndex = 244;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(837, 245);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 43);
+            this.btnAdd.TabIndex = 243;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txtdescription
+            // 
+            this.txtdescription.Location = new System.Drawing.Point(1083, 183);
+            this.txtdescription.Name = "txtdescription";
+            this.txtdescription.Size = new System.Drawing.Size(343, 22);
+            this.txtdescription.TabIndex = 242;
+            // 
+            // lbldescription
+            // 
+            this.lbldescription.AutoSize = true;
+            this.lbldescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldescription.Location = new System.Drawing.Point(875, 176);
+            this.lbldescription.Name = "lbldescription";
+            this.lbldescription.Size = new System.Drawing.Size(146, 29);
+            this.lbldescription.TabIndex = 241;
+            this.lbldescription.Text = "Description";
+            // 
+            // txtproductid
+            // 
+            this.txtproductid.Location = new System.Drawing.Point(531, 183);
+            this.txtproductid.Name = "txtproductid";
+            this.txtproductid.Size = new System.Drawing.Size(257, 22);
+            this.txtproductid.TabIndex = 240;
+            // 
+            // lblproductid
+            // 
+            this.lblproductid.AutoSize = true;
+            this.lblproductid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblproductid.Location = new System.Drawing.Point(327, 176);
+            this.lblproductid.Name = "lblproductid";
+            this.lblproductid.Size = new System.Drawing.Size(132, 29);
+            this.lblproductid.TabIndex = 239;
+            this.lblproductid.Text = "Product Id";
+            // 
+            // txtproductname
+            // 
+            this.txtproductname.Location = new System.Drawing.Point(531, 252);
+            this.txtproductname.Name = "txtproductname";
+            this.txtproductname.Size = new System.Drawing.Size(257, 22);
+            this.txtproductname.TabIndex = 238;
+            // 
+            // lblproductname
+            // 
+            this.lblproductname.AutoSize = true;
+            this.lblproductname.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblproductname.Location = new System.Drawing.Point(327, 245);
+            this.lblproductname.Name = "lblproductname";
+            this.lblproductname.Size = new System.Drawing.Size(179, 29);
+            this.lblproductname.TabIndex = 237;
+            this.lblproductname.Text = "Product Name";
+            // 
+            // btnproduct
+            // 
+            this.btnproduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnproduct.Location = new System.Drawing.Point(12, 337);
+            this.btnproduct.Name = "btnproduct";
+            this.btnproduct.Size = new System.Drawing.Size(190, 39);
+            this.btnproduct.TabIndex = 236;
+            this.btnproduct.Text = "Product";
+            this.btnproduct.UseVisualStyleBackColor = true;
+            // 
+            // btnimage
+            // 
+            this.btnimage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnimage.Image = ((System.Drawing.Image)(resources.GetObject("btnimage.Image")));
+            this.btnimage.Location = new System.Drawing.Point(12, 12);
+            this.btnimage.Name = "btnimage";
+            this.btnimage.Size = new System.Drawing.Size(207, 160);
+            this.btnimage.TabIndex = 235;
+            this.btnimage.UseVisualStyleBackColor = true;
+            // 
+            // lblproductdetails
+            // 
+            this.lblproductdetails.AutoSize = true;
+            this.lblproductdetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblproductdetails.Location = new System.Drawing.Point(686, 49);
+            this.lblproductdetails.Name = "lblproductdetails";
+            this.lblproductdetails.Size = new System.Drawing.Size(232, 36);
+            this.lblproductdetails.TabIndex = 234;
+            this.lblproductdetails.Text = "Product Details";
+            // 
+            // lbleshift
+            // 
+            this.lbleshift.AutoSize = true;
+            this.lbleshift.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleshift.Location = new System.Drawing.Point(253, 17);
+            this.lbleshift.Name = "lbleshift";
+            this.lbleshift.Size = new System.Drawing.Size(275, 91);
+            this.lbleshift.TabIndex = 233;
+            this.lbleshift.Text = "e-Shift";
+            // 
+            // btnreport
+            // 
+            this.btnreport.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreport.Location = new System.Drawing.Point(12, 585);
+            this.btnreport.Name = "btnreport";
+            this.btnreport.Size = new System.Drawing.Size(190, 39);
+            this.btnreport.TabIndex = 232;
+            this.btnreport.Text = "Report";
+            this.btnreport.UseVisualStyleBackColor = true;
+            // 
+            // btntransunit
+            // 
+            this.btntransunit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntransunit.Location = new System.Drawing.Point(12, 525);
+            this.btntransunit.Name = "btntransunit";
+            this.btntransunit.Size = new System.Drawing.Size(190, 39);
+            this.btntransunit.TabIndex = 231;
+            this.btntransunit.Text = "Transport Unit";
+            this.btntransunit.UseVisualStyleBackColor = true;
+            // 
+            // btnload
+            // 
+            this.btnload.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnload.Location = new System.Drawing.Point(12, 462);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(190, 39);
+            this.btnload.TabIndex = 230;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            // 
+            // btnjob
+            // 
+            this.btnjob.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnjob.Location = new System.Drawing.Point(12, 395);
+            this.btnjob.Name = "btnjob";
+            this.btnjob.Size = new System.Drawing.Size(190, 39);
+            this.btnjob.TabIndex = 229;
+            this.btnjob.Text = "Transport Job";
+            this.btnjob.UseVisualStyleBackColor = true;
+            // 
+            // btncustomer
+            // 
+            this.btncustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncustomer.Location = new System.Drawing.Point(12, 269);
+            this.btncustomer.Name = "btncustomer";
+            this.btncustomer.Size = new System.Drawing.Size(190, 39);
+            this.btncustomer.TabIndex = 228;
+            this.btncustomer.Text = "Customer";
+            this.btncustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnadmin
+            // 
+            this.btnadmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadmin.Location = new System.Drawing.Point(12, 207);
+            this.btnadmin.Name = "btnadmin";
+            this.btnadmin.Size = new System.Drawing.Size(190, 39);
+            this.btnadmin.TabIndex = 227;
+            this.btnadmin.Text = "Admin";
+            this.btnadmin.UseVisualStyleBackColor = true;
+            this.btnadmin.Click += new System.EventHandler(this.btnadmin_Click);
+            // 
+            // Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1505, 721);
+            this.Controls.Add(this.dataGridViewproductdetails);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtdescription);
+            this.Controls.Add(this.lbldescription);
+            this.Controls.Add(this.txtproductid);
+            this.Controls.Add(this.lblproductid);
+            this.Controls.Add(this.txtproductname);
+            this.Controls.Add(this.lblproductname);
+            this.Controls.Add(this.btnproduct);
+            this.Controls.Add(this.btnimage);
+            this.Controls.Add(this.lblproductdetails);
+            this.Controls.Add(this.lbleshift);
+            this.Controls.Add(this.btnreport);
+            this.Controls.Add(this.btntransunit);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.btnjob);
+            this.Controls.Add(this.btncustomer);
+            this.Controls.Add(this.btnadmin);
+            this.Name = "Product";
+            this.Text = "Product";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewproductdetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewproductdetails;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtdescription;
+        private System.Windows.Forms.Label lbldescription;
+        private System.Windows.Forms.TextBox txtproductid;
+        private System.Windows.Forms.Label lblproductid;
+        private System.Windows.Forms.TextBox txtproductname;
+        private System.Windows.Forms.Label lblproductname;
+        private System.Windows.Forms.Button btnproduct;
+        private System.Windows.Forms.Button btnimage;
+        private System.Windows.Forms.Label lblproductdetails;
+        private System.Windows.Forms.Label lbleshift;
+        private System.Windows.Forms.Button btnreport;
+        private System.Windows.Forms.Button btntransunit;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btnjob;
+        private System.Windows.Forms.Button btncustomer;
+        private System.Windows.Forms.Button btnadmin;
+    }
+}
