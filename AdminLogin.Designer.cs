06ad92995bc1116a312eebc78fe81593a3f3f959
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:10:52 +0530

    Add files via upload

---
+++ AdminLogin.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace EShift_transport
+{
+    partial class AdminLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminLogin));
+            this.lbleshift = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtuserid = new System.Windows.Forms.TextBox();
+            this.btnadminsignup = new System.Windows.Forms.Button();
+            this.lbluserid = new System.Windows.Forms.Label();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.lbladlogin = new System.Windows.Forms.Label();
+            this.btnadminlogin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbleshift
+            // 
+            this.lbleshift.AutoSize = true;
+            this.lbleshift.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleshift.Location = new System.Drawing.Point(580, 49);
+            this.lbleshift.Name = "lbleshift";
+            this.lbleshift.Size = new System.Drawing.Size(275, 91);
+            this.lbleshift.TabIndex = 13;
+            this.lbleshift.Text = "e-Shift";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(1112, 383);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(222, 22);
+            this.txtusername.TabIndex = 23;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(1112, 475);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(222, 22);
+            this.txtpassword.TabIndex = 22;
+            // 
+            // txtuserid
+            // 
+            this.txtuserid.Location = new System.Drawing.Point(1112, 301);
+            this.txtuserid.Name = "txtuserid";
+            this.txtuserid.Size = new System.Drawing.Size(222, 22);
+            this.txtuserid.TabIndex = 21;
+            // 
+            // btnadminsignup
+            // 
+            this.btnadminsignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadminsignup.Location = new System.Drawing.Point(1171, 597);
+            this.btnadminsignup.Name = "btnadminsignup";
+            this.btnadminsignup.Size = new System.Drawing.Size(130, 46);
+            this.btnadminsignup.TabIndex = 20;
+            this.btnadminsignup.Text = "Sign Up";
+            this.btnadminsignup.UseVisualStyleBackColor = true;
+            // 
+            // lbluserid
+            // 
+            this.lbluserid.AutoSize = true;
+            this.lbluserid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluserid.Location = new System.Drawing.Point(889, 294);
+            this.lbluserid.Name = "lbluserid";
+            this.lbluserid.Size = new System.Drawing.Size(97, 29);
+            this.lbluserid.TabIndex = 19;
+            this.lbluserid.Text = "User Id";
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblusername.Location = new System.Drawing.Point(889, 376);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(144, 29);
+            this.lblusername.TabIndex = 18;
+            this.lblusername.Text = "User Name";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpassword.Location = new System.Drawing.Point(889, 468);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(128, 29);
+            this.lblpassword.TabIndex = 17;
+            this.lblpassword.Text = "Password";
+            // 
+            // lbladlogin
+            // 
+            this.lbladlogin.AutoSize = true;
+            this.lbladlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbladlogin.Location = new System.Drawing.Point(1054, 176);
+            this.lbladlogin.Name = "lbladlogin";
+            this.lbladlogin.Size = new System.Drawing.Size(95, 36);
+            this.lbladlogin.TabIndex = 16;
+            this.lbladlogin.Text = "Login";
+            // 
+            // btnadminlogin
+            // 
+            this.btnadminlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadminlogin.Location = new System.Drawing.Point(942, 597);
+            this.btnadminlogin.Name = "btnadminlogin";
+            this.btnadminlogin.Size = new System.Drawing.Size(130, 46);
+            this.btnadminlogin.TabIndex = 15;
+            this.btnadminlogin.Text = "Login";
+            this.btnadminlogin.UseVisualStyleBackColor = true;
+            this.btnadminlogin.Click += new System.EventHandler(this.btnadminlogin_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(61, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(527, 456);
+            this.button1.TabIndex = 14;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AdminLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1504, 713);
+            this.Controls.Add(this.lbleshift);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtuserid);
+            this.Controls.Add(this.btnadminsignup);
+            this.Controls.Add(this.lbluserid);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.lblpassword);
+            this.Controls.Add(this.lbladlogin);
+            this.Controls.Add(this.btnadminlogin);
+            this.Controls.Add(this.button1);
+            this.Name = "AdminLogin";
+            this.Text = "AdminLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbleshift;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.TextBox txtuserid;
+        private System.Windows.Forms.Button btnadminsignup;
+        private System.Windows.Forms.Label lbluserid;
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.Label lbladlogin;
+        private System.Windows.Forms.Button btnadminlogin;
+        private System.Windows.Forms.Button button1;
+    }
+}
