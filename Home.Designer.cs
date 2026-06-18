a22037bb01d40183b178b18cee67e122f461f61c
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:25:22 +0530

    Add files via upload

---
+++ Home.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace EShift_transport
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.btnadmin = new System.Windows.Forms.Button();
+            this.btncustomer = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnadmin
+            // 
+            this.btnadmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadmin.Location = new System.Drawing.Point(1108, 344);
+            this.btnadmin.Name = "btnadmin";
+            this.btnadmin.Size = new System.Drawing.Size(181, 47);
+            this.btnadmin.TabIndex = 7;
+            this.btnadmin.Text = "Admin";
+            this.btnadmin.UseVisualStyleBackColor = true;
+            this.btnadmin.Click += new System.EventHandler(this.btnadmin_Click);
+            // 
+            // btncustomer
+            // 
+            this.btncustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncustomer.Location = new System.Drawing.Point(839, 340);
+            this.btncustomer.Name = "btncustomer";
+            this.btncustomer.Size = new System.Drawing.Size(181, 51);
+            this.btncustomer.TabIndex = 6;
+            this.btncustomer.Text = "Customer ";
+            this.btncustomer.UseVisualStyleBackColor = true;
+            this.btncustomer.Click += new System.EventHandler(this.btncustomer_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(138, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(527, 456);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(650, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 91);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "e-Shift";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1515, 701);
+            this.Controls.Add(this.btnadmin);
+            this.Controls.Add(this.btncustomer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnadmin;
+        private System.Windows.Forms.Button btncustomer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
