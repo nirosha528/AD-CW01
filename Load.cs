c8fc2192a5edaeada82394971af054ac4af855cf
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-18 18:26:35 +0530

    Add files via upload

---
+++ Load.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EShift_transport
+{
+    public partial class Load : Form
+    {
+        public Load()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadmin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdminHome home = new AdminHome();
+            home.Show();
+        }
+
+        private void btncustomer_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CustomerDetails customer = new CustomerDetails();
+            customer.Show();
+        }
+    }
+}
